6c0ebd831d5571a0d49c04234f2135ece7bc0bb6
Krishnakumar Boggaram <krishnakumar.boggaram@ey.com>
2021-10-23 12:04:24 +0530

    Added login component

--- ExampleApplication/ExampleApplication/ClientApp/src/app/app.module.ts
+++ ExampleApplication/ExampleApplication/ClientApp/src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+     RouterModule.forRoot([
+      { path: '', component:LoginComponent , pathMatch: 'full' },
+       /* { path: 'first', component: FirstComponentComponent },
+      { path: 'second', component: NewSecondComponent },  */
+    ]) 
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
