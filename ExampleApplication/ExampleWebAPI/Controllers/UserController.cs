fafe36485428cc17a49d5f83d588cf46261e9319
saikamalteja <tejasaikamal@gmail.com>
2021-10-24 10:18:40 +0530

    add data to github

--- ExampleApplication/ExampleWebAPI/Controllers/UserController.cs
+++ ExampleApplication/ExampleWebAPI/Controllers/UserController.cs
@@ -2,16 +2,165 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ExampleWebAPI.Controllers
 {
+    public class UserProfile
+    {
+        public string UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+
+        public string UserPassword { get; set; }
+        public string DateOfBirth { get; set; }
+        public string CommunicationAddress { get; set; }
+        public string EmailId { get; set; }
+        public string MobileNumber { get; set; }
+        public string Interests { get; set; }
+    }
+
+
+
+
+
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        [Route("GetUserProfile")]
+        [HttpGet]
+        public IActionResult GetUserProfile()
+        {
+            List<UserProfile> myUserProfile = new List<UserProfile>();
+            try
+            {
+                string ConnectionString = "Data Source = LAPTOP-QP79NHVA\\SQLEXPRESS; Initial Catalog = DotNet; " + "Integrated Security = true";
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    SqlCommand command = new SqlCommand("select * from UserProfile", conn);
+                    SqlDataReader reader = command.ExecuteReader();
+                    int nCount = 0;
+                    while (reader.Read())
+                    {
 
+                        UserProfile up = new UserProfile();
+                        up.UserId = reader["UserId"].ToString();
+                        up.UserFirstName = reader["UserFirstName"].ToString();
+                        up.UserLastName = reader["UserLastName"].ToString();
+                        up.UserPassword = reader["UserPassword"].ToString();
+                        up.DateOfBirth = reader["DateOfBirth"].ToString();
+                        up.CommunicationAddress = reader["CommunicationAddress"].ToString();
+                        up.EmailId = reader["EmailId"].ToString();
+                        up.MobileNumber = reader["MobileNumber"].ToString();
+                        up.Interests = reader["Interests"].ToString();
+
+
+                        nCount++;
+                        myUserProfile.Add(up);
+
+                    }
+                    conn.Close();
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hi... There is an exception:. " + ex.Message);
+            }
+            return Ok(myUserProfile);
+        }
+
+
+
+
+
+
+        //PostMethod
+
+        [Route("InsertUserProfile")]
+        [HttpPost]
+        public IActionResult InsertUserProfile([FromBody] List<UserProfile> myUserProfile)
+        {
+            try
+            {
+                string ConnectionString = "Data Source = LAPTOP-QP79NHVA\\SQLEXPRESS; Initial Catalog = DotNet; " + "Integrated Security = true";
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    int recordsInserted = 0;
+                    for (int i = 0; i < myUserProfile.Count; i++)
+                    {
+                        string commandText = $"INSERT INTO UserProfile VALUES('{myUserProfile[i].UserId}','{myUserProfile[i].UserFirstName}', '{myUserProfile[i].UserLastName}', '{myUserProfile[i].UserPassword}', '{myUserProfile[i].DateOfBirth}','{myUserProfile[i].CommunicationAddress}','{myUserProfile[i].EmailId}','{myUserProfile[i].MobileNumber}','{myUserProfile[i].Interests}')";
+                        SqlCommand command = new SqlCommand(commandText, conn);
+
+                        command.ExecuteNonQuery();
+
+                        recordsInserted++;
+                    }
+                    conn.Close();
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+            return Ok();
+
+        }
+
+
+
+        //put
+
+        [Route("UpdateUserProfile")]
+        [HttpPost]
+        public IActionResult UpdateUserProfile([FromBody] List<UserProfile> myUserProfile)
+        {
+            try
+            {
+                string ConnectionString = "Data Source = LAPTOP-QP79NHVA\\SQLEXPRESS; Initial Catalog = DotNet; " + "Integrated Security = true";
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    int recordsInserted = 0;
+                    for (int i = 0; i < myUserProfile.Count; i++)
+                    {
+                        string commandText = $"UPDATE UserProfile VALUES('{myUserProfile[i].UserId}','{myUserProfile[i].UserFirstName}', '{myUserProfile[i].UserLastName}', '{myUserProfile[i].UserPassword}', '{myUserProfile[i].DateOfBirth}','{myUserProfile[i].CommunicationAddress}','{myUserProfile[i].EmailId}','{myUserProfile[i].MobileNumber}','{myUserProfile[i].Interests}')";
+                        SqlCommand command = new SqlCommand(commandText, conn);
+
+                        command.ExecuteNonQuery();
+
+                        recordsInserted++;
+                    }
+                    conn.Close();
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+            return Ok();
+
+        }
 
     }
+
 }
+     
+
+
+
--- ExampleApplication/SQLItems/DataTables.sql
+++ ExampleApplication/SQLItems/DataTables.sql
@@ -1,1 +1,11 @@
-select * from UserProfile
+CREATE TABLE UserProfile (
+    UserId varchar(10),
+    UserFirstName varchar(100),
+    UserLastName  varchar(100),
+	UserPassword varchar(250),
+	DateOfBirth varchar(100),
+	CommunicationAddress varchar(400),
+	EmailId varchar(100),
+	MobileNumber varchar(15),
+	Interests varchar(500),	   
+);
